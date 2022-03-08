f451ef4bd7bb4b98d25e393198f1722ed7c209ab
rayan.garcia <rayan.garcia@efabrika.com.br>
2022-03-08 15:58:38 -0300

    updating discord librarie

--- src/main/java/com/treta/bot/config/BotConfiguration.java
+++ src/main/java/com/treta/bot/config/BotConfiguration.java
@@ -1,15 +1,12 @@
 package com.treta.bot.config;
 
-import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
-import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
 import com.sedmelluq.discord.lavaplayer.track.playback.NonAllocatingAudioFrameBuffer;
 import com.treta.bot.listener.EventListener;
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.Event;
-import discord4j.voice.AudioProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -28,17 +25,14 @@ public class BotConfiguration {
 
     @Bean
     public <T extends Event> GatewayDiscordClient gatewayDiscordClient (List<EventListener<T>> eventListeners) {
-
         playerManager.getConfiguration().setFrameBufferFactory(NonAllocatingAudioFrameBuffer::new);
         AudioSourceManagers.registerRemoteSources(playerManager);
-
         GatewayDiscordClient client = DiscordClientBuilder.create(token)
                 .build()
                 .login()
                 .block();
 
         for (EventListener<T> listener : eventListeners) {
-
             client.on(listener.getEventType())
                     .flatMap(listener::execute)
                     .onErrorResume(listener::handleError)
